46999c040a4239fe58046de6fa8a9c1bf429ce24
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-04-05 23:26:48 +0530

    Create 9.Practice-2.py

---
+++ 9.Practice-2.py
@@ -0,0 +1,24 @@
+#1.Write Program to sum of two number
+# a=10
+# b=20
+# print(a+b)
+
+#2.Write code to find reminder when a number is devided by 2
+# c=a%b
+# print(c)
+
+#3.Write program to one value is bigger then second or not
+# d= int(input("Enter Value of a: "))
+# e= int(input("Enter Value of b: "))
+
+# print("A is greater than b is: ",a>b)
+
+#3.Write Program to find average of two number
+# a= int(input("Enter Value of a: "))
+# b= int(input("Enter Value of b: "))
+
+# print("Average of two number is: ",(a+b)/2)
+
+#3.Write program to chalculate the square rool of the number
+# a =float(input("Enter number: "))
+# print("The Square Root of number is: ",a**2)
