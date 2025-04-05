cd4a669fa234f8293695c1ac30f31707c4bcb81a
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-04-05 22:37:36 +0530

    Create 5.DataType.py

---
+++ 5.DataType.py
@@ -0,0 +1,12 @@
+#Data Type
+a=1;                #integer
+b=5.22;             #Float
+c="Rathod Pratik"   #String
+d=False;            #Boolean
+e=None              #none
+
+print(type(a))
+print(type(b))
+print(type(b))
+print(type(d))
+print(type(e))
