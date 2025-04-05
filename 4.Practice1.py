6aa2f14984dd03c8509791f428c3299d45f9296f
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-04-05 21:43:54 +0530

    Create 4.Practice1.py

---
+++ 4.Practice1.py
@@ -0,0 +1,28 @@
+# 1.Print Multiple Line
+# print('''
+#       Im print multiple
+#             Line 
+#            using 
+#         Single quote
+# ''');
+
+
+#2.Install externl module and use it in Program
+# import pyttsx3
+
+# engine =pyttsx3.init()
+# engine.say("Hey Rathod Pratik Whats going on");
+# engine.runAndWait()
+
+#3.Print Files in Directory
+#Import the Os module
+# import os
+
+#Select the directory
+# directory_path="/"
+
+#Call function and store in variable
+# contents=os.listdir(directory_path);
+
+#Print Variable using loop
+#print(contents)
