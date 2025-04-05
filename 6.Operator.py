3873a9e80a8779da38ce311a5d306f976db47700
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-04-05 23:08:56 +0530

    Create 6.Operator.py

---
+++ 6.Operator.py
@@ -0,0 +1,33 @@
+print("1.Arithmetic Operator")
+print(30+10)
+print(30-10)
+print(30*10)
+print(30/10)
+print(30%10)
+
+print("2.Comparison Operator")
+a=10
+b=5
+print(a<b)
+print(a>b)
+print(a>=b)
+print(a<=b)
+print(a!=b)
+print(a==b)
+
+print("3.Logical Operator")
+print(True or False)
+print(True and False)
+print(not False)
+
+print("4.Assignment Operator")
+a=10
+print(a)
+a+=2
+print(a)
+a-=5
+print(a)
+a*=10
+print(a)
+a/=2
+print(a)
