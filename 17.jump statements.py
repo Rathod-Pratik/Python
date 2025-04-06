320510ca268629de42e8eb29266d272c67de9e02
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-04-06 18:09:41 +0530

    Create 17.jump statements.py

---
+++ 17.jump statements.py
@@ -0,0 +1,15 @@
+#1.Break use to exit loop
+# for i in range(10):
+#     if(i==7):
+#         break
+#     print(i)
+
+# 2.pass use to Executen nothing
+for i in range(10):
+   pass
+
+# 3.Continue use to skip iteration
+for i in range(10):
+    if(i==5):
+        continue
+    print(i)
