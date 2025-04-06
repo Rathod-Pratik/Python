089ab2235be96c2ed1772f4e87c6a4742588a35a
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-04-06 16:45:30 +0530

    Update 15.Conditional_Statement.py

--- 15.Conditional_Statement.py
+++ 15.Conditional_Statement.py
@@ -1,7 +1,24 @@
 age=int(input("Enter your age: "))
 
-if(age<18):
-    print("You are not eligible for vote")
+# 1.If condition
+# if(age<18):
+#     print("You are not eligible for vote")
 
+
+# # 2.If else condition
+# if(age<18):
+#     print("You are not eligible for vote")
+
+# else:
+#     print("You are eligible for vote")
+
+
+# 3.If else Leadder condition
+if(age<0):
+    print("You enter nagative age")
+elif(age==0):
+    print("You enter zero Which is not valid")
+elif(age<18):
+    print("You are not eligible for vote")
 else:
     print("You are eligible for vote")
