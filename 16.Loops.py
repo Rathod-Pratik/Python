86fcda34bc8e725d2832d6150c3f168463a66d9c
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-04-06 18:09:28 +0530

    Create 16.Loops.py

---
+++ 16.Loops.py
@@ -0,0 +1,12 @@
+# We can use Loop to print List,touple and sets
+Name=["Mohit","Anish","Krish","Karan","Dax"]
+
+# i=0
+# while(i<len(Name)):
+#     print(Name[i])
+#     i+=1
+
+for i in range(len(Name)):
+    print(Name[i])
+else:
+    print("Name printed Successfully")
