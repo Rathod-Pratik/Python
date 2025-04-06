16d0157686f056f8ab32d2b0e33284ddc8951087
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-04-06 12:33:55 +0530

    Create 12.Touple.py

---
+++ 12.Touple.py
@@ -0,0 +1,27 @@
+a=(15,15,80,89,77,84,49,16,49,84)
+# Print Touple
+print(a)    
+
+# Print type
+print(type(a)) 
+
+#count Value
+print(a.count(84))
+
+#Return index
+print(a.index(80))
+
+#find the length
+print(len(a))
+
+#find the Max value
+print(max(a))
+
+#finnd the min value
+print(min(a))
+
+#Find sum of all number
+print(sum(a))
+
+#Short the value
+print(sorted(a))
