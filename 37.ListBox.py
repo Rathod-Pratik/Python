39159f9b189db00f06fd0be42656c533395e3679
Rathod Pratik <rathodpratik1928@gmail.com>
2025-06-21 18:29:25 +0530

    Create 37.ListBox.py with basic Tkinter Listbox setup

--- 37.ListBox.py
+++ 37.ListBox.py
@@ -2,7 +2,7 @@ from tkinter import *
 r=Tk()
 r.geometry()
 
-l=Listbox(r,height=5,width=20,bg='pink',fg='red',font='pristina',activestyle=None)
+l=Listbox(r,height=5,width=20,bg='pink',fg='red',font=("batang",18,"bold"),activestyle=None)
 l.insert(1,"Pizza")
 l.insert(2,"Burger")
 l.insert(3,"Chinse")
